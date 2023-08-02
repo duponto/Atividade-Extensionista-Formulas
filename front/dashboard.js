705d7bc7a479231a3fcc5d0ca226762b6f92303d
Gabriel Dupont Muenchen <gabriel.327m@gmail.com>
2023-08-02 01:15:39 -0300

    configurando metodos pros valores do grafico

--- front/dashboard.js
+++ front/dashboard.js
@@ -1,54 +1,30 @@
 /* globals Chart:false, feather:false */
-
-(function () {
-    'use strict'
-  
-    feather.replace()
-  
-    // Graphs
-    var ctx = document.getElementById('myChart')
-    // eslint-disable-next-line no-unused-vars
-    var myChart = new Chart(ctx, {
-      type: 'line',
-      data: {
-        labels: [
-          'Sunday',
-          'Monday',
-          'Tuesday',
-          'Wednesday',
-          'Thursday',
-          'Friday',
-          'Saturday'
-        ],
-        datasets: [{
-          data: [
-            15339,
-            21345,
-            18483,
-            24003,
-            23489,
-            24092,
-            12034
-          ],
-          lineTension: 0,
-          backgroundColor: 'transparent',
-          borderColor: '#007bff',
-          borderWidth: 4,
-          pointBackgroundColor: '#007bff'
-        }]
-      },
-      options: {
-        scales: {
-          yAxes: [{
-            ticks: {
-              beginAtZero: true
-            }
-          }]
-        },
-        legend: {
-          display: false
+import objValues from './graphValues.js';
+var ctx = document.getElementById('myChart')
+var myChart = new Chart(ctx, {
+  type: 'line',
+  data: {
+    labels: objValues.dates,
+    datasets: [{
+      data: objValues.values,
+      lineTension: 0,
+      backgroundColor: 'transparent',
+      borderColor: '#007bff',
+      borderWidth: 4,
+      pointBackgroundColor: '#007bff'
+    }]
+  },
+  options: {
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero: true
         }
-      }
-    })
-  }())
+      }]
+    },
+    legend: {
+      display: false
+    }
+  }
+});
   
---
+++ front/graphValues.js
@@ -0,0 +1,19 @@
+const startDate = new Date(2022, 0); // Janeiro de 2022
+const endDate = new Date(2024, 11); // Dezembro de 2024
+const objValues = {
+    dates: [],
+    values: [],
+};
+const currentDate = new Date(startDate);
+while (currentDate <= endDate) {
+    const year = currentDate.getFullYear();
+    const month = currentDate.getMonth() + 1;
+    objValues.dates.push(`${year}-${month.toString().padStart(2, '0')}`);
+
+    objValues.values.push(Math.floor(Math.random() * 100) + 1);
+    currentDate.setMonth(currentDate.getMonth() + 1);
+}
+
+console.log(objValues)
+
+export default objValues;
